8386c7286a226c6feb12fb477af186e5a8f767d3
billyott <willrichott@gmail.com>
2020-09-16 13:36:02 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,104 @@
 # your code goes here
+# Introduction
+# To practice object oriented programming (OOP), you're going to create a Person class. Each instance of the Person class will have the ablity to: - get paid/receive payments - take a bath - call a friend - start a conversation - state if they are happy and/or clean
+
+# Instructions
+# First you need to create a person class that is initialized with a name that cannot be changed.
+# Each instance of class Personshould be able to remember their name
+# Each instance of class Person should start with $25 in their bank accounts
+# Each instance of class Person should start with eight happiness points
+# Each instance of class Person should start with eight hygiene points
+# The happiness and hygiene points should be able to change, however the maximum and minimum points for both happiness and hygiene should be 10 and 0 respectively
+# The amount in the bank account should also be able to change, though it has no max or min.
+
+class Person
+
+    attr_accessor :bank_account
+    attr_reader :name, :happiness, :hygiene
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+
+    def happiness=(new_happiness)
+
+        if new_happiness > 10
+            @happiness = 10
+        elsif new_happiness < 0
+            @happiness = 0
+        else
+            @happiness = new_happiness
+        end
+    end
+
+    def hygiene=(new_hygiene)
+        if new_hygiene > 10
+            @hygiene = 10
+        elsif new_hygiene < 0
+            @hygiene = 0
+        else
+            @hygiene = new_hygiene
+        end
+    end
+
+    def happy?
+        if @happiness > 7
+            return true
+        else
+            return false
+        end
+    end
+
+    def clean?
+        if @hygiene > 7
+            return true
+        else
+            return false
+        end
+    end
+
+    def get_paid(salary)
+        @bank_account += salary
+        return "all about the benjamins"
+    end
+
+    def take_bath
+        hygiene_points = @hygiene += 4
+        self.hygiene=(hygiene_points)
+        return "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        self.hygiene -= 3
+        self.happiness += 2
+        return "♪ another one bites the dust ♫"
+    end
+
+
+    def call_friend(friend)
+        self.happiness += 3
+        friend.happiness += 3
+        "Hi #{friend.name}! It's #{@name}. How are you?"
+    end
+
+    def start_conversation(friend,topic)
+        if topic == 'politics'
+            self.happiness -= 2
+            friend.happiness -= 2
+            return 'blah blah partisan blah lobbyist'
+        elsif topic == 'weather'
+            self.happiness += 1           
+            friend.happiness += 1
+            return 'blah blah sun blah rain'
+        else topic == 'other'
+            return 'blah blah blah blah blah'
+        end
+
+    end
+
+
+end
